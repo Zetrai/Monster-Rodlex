e870ac2820926405c96c8793917c8c9438c6ac97
zetra <karanpatadia28@gmail.com>
2022-10-08 00:33:03 +0530

    setState Beginner code

--- monster-rodlex/src/App.js
+++ monster-rodlex/src/App.js
@@ -1,25 +1,48 @@
+import { Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: { firstName: 'Zetra', lastName: 'Orion' },
+      company: 'Wipro',
+    };
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <p>
+            Hello {this.state.name.firstName} {this.state.name.lastName}, I Work
+            at {this.state.company}
+          </p>
+          <button
+            onClick={() => {
+              const firstName =
+                this.state.name.firstName === 'Artez' ? 'Zetra' : 'Artez';
+              this.setState(
+                (state, props) => {
+                  return { name: { firstName: firstName, lastName: 'Orion' } };
+                },
+                () => {
+                  // only when state is updated execute following code. Its a callback function
+                  console.log(this.state);
+                }
+              );
+            }}
+          >
+            Change Name
+          </button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
