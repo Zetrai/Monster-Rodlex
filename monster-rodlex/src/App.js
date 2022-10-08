7883f7330b5f14c9ec9844a276463a5564419a99
zetra <karanpatadia28@gmail.com>
2022-10-08 15:41:14 +0530

    commit-11: Adding hook - useEffect to fetch  Names

--- monster-rodlex/src/App.js
+++ monster-rodlex/src/App.js
@@ -7,15 +7,29 @@ import './App.css';
 const App = () => {
   const [searchTarget, setSearchTarget] = useState(''); // [value, setValue] useState(initialValue)
   const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
+
+  // First is callback that is effect that we want to trigger inside our functional component
+  // Second is the dependencies like monsters or any props
+  // Callback is called only when dependencies inside the array changes.
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => setMonsters(users));
+  }, []);
+
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchTarget);
+    });
+    setFilteredMonsters(newFilteredMonsters);
+  }, [monsters, searchTarget]);
 
   const onSearchChange = (event) => {
     const searchTargetString = event.target.value.toLocaleLowerCase();
     setSearchTarget(searchTargetString);
   };
 
-  const filteredMonsters = monsters.filter((monster) => {
-    return monster.name.toLocaleLowerCase().includes(searchTarget);
-  });
   return (
     <div className='App'>
       <h1 className='app-title'>Monsters Rodlex</h1>
